9879d0a779299cbc6c9640597f5f2c3730d37070
ajlopez <webmaster@ajlopez.com>
2018-06-23 17:31:10 -0300

    TicTacToe create board with some pieces

--- ejemplos/rlttt/lib/ttt.js
+++ ejemplos/rlttt/lib/ttt.js
@@ -1,12 +1,32 @@
 
-function TicTacToe() {
-	this.width = function () { return 3; };
-	this.height = function () { return 3;};
-	this.toString = function () { return '         '; };
+function TicTacToe(initial) {
+	initial = initial || '';
+	var board = [ [], [], [] ];
+	
+	fromString(initial);
+
+	this.width = function () { return board.length; };
+	this.height = function () { return board[0].length;};
+	
+	this.get = function (x, y) { return board[x][y]; };
+	
+	this.toString = function () {
+		var result = '';
+		
+		for (var x = 0; x < 3; x++)
+			result += board[x].join('');
+		
+		return result;
+	}
+	
+	function fromString(initial) {
+		for (k = 0; k < 3 * 3; k++)
+			board[Math.floor(k / 3)][k % 3] = initial[k] == null ? ' ' : initial[k];
+	}
 }
 
-function createTicTacToe() {
-	return new TicTacToe();
+function createTicTacToe(initial) {
+	return new TicTacToe(initial);
 }
 
 module.exports = createTicTacToe;
--- ejemplos/rlttt/test/ttt.js
+++ ejemplos/rlttt/test/ttt.js
@@ -11,3 +11,26 @@ exports['create empty board'] = function (test) {
 	test.equal(board.toString(), '         ');
 };
 
+exports['create board with some pieces'] = function (test) {
+	var board = ttt('OO  X XXO');
+	
+	test.ok(board);
+	test.equal(typeof board, 'object');
+	test.equal(board.width(), 3);
+	test.equal(board.height(), 3);
+	
+	test.equal(board.get(0,0), 'O');
+	test.equal(board.get(0,1), 'O');
+	test.equal(board.get(0,2), ' ');
+	
+	test.equal(board.get(1,0), ' ');
+	test.equal(board.get(1,1), 'X');
+	test.equal(board.get(1,2), ' ');
+	
+	test.equal(board.get(2,0), 'X');
+	test.equal(board.get(2,1), 'X');
+	test.equal(board.get(2,2), 'O');
+
+	test.equal(board.toString(), 'OO  X XXO');
+};
+
